09edc5b8f92d735f4450f272f776c00be5ff49aa
Egor Shatrov <eshatrov@itgua.com>
2019-02-28 16:27:18 +0200

    Problem 16 solved

--- Problem16.py
+++ Problem16.py
@@ -1,5 +1,14 @@
 # Problem 16 
-# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-# What is the sum of the digits of the number 21000?
+# What is the sum of the digits of the number 2^1000?
 
+number = 2**1000
+sum = 0
+
+num = str(number)
+
+for i in num:
+    sum = sum + int(i)
+
+print(sum)
