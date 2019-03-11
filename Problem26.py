4f1bba264de838fc49ee5a32a79bb1f020eb8132
Egor Shatrov <xutpuu@gmail.com>
2019-03-11 23:45:16 +0200

    Problem 26 begin

--- Problem26.py
+++ Problem26.py
@@ -12,3 +12,4 @@
 # Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
 
 # Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
