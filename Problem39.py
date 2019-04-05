c2aff99e26efea1f13ded9f102caa98ad6c6ae04
Egor Shatrov <eshatrov@itgua.com>
2019-04-05 15:41:08 +0300

    clean problem 39

--- Problem39.py
+++ Problem39.py
@@ -1,15 +1,7 @@
 # If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
 # {20,48,52}, {24,45,51}, {30,40,50}
 # For which value of p ≤ 1000, is the number of solutions maximised?
-import math
 import time as t
-
-
-def per_RightAngleTriangle(a, b, c):
-    p = 120
-    return p * (p - 2 * a) % (2 * (p - a))
-
-
 
 # a² + b² = c²
 # a + b + c = p
@@ -36,7 +28,6 @@ def program():
 def main():
     start = t.time()
     program()
-    print(per_RightAngleTriangle(20,48,52))
     end = t.time()
     print("Total time taken:", end - start, "ms")
 
