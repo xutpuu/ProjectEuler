360a5357176f861b49ca69a5b9b7889dcb971ef3
Egor Shatrov <eshatrov@itgua.com>
2019-04-05 15:40:42 +0300

    Problem 39 solved

--- Problem39.py
+++ Problem39.py
@@ -6,28 +6,37 @@ import time as t
 
 
 def per_RightAngleTriangle(a, b, c):
-    s = a**2 + b**2
-    if c == math.sqrt(s):
-        return a + b + c
-    else:
-        return 0
+    p = 120
+    return p * (p - 2 * a) % (2 * (p - a))
 
 
+
+# a² + b² = c²
+# a + b + c = p
+# c = p - a - b
+# a² + b² = (p - a - b)² = p² + a² + b² - 2pa - 2pb + 2ab
+# b = (p² - 2pa) / (2p - 2a)
+# a < c and b < c
+# a ≤ b < c 
 def program():
-    limit = 100
-    counter = 0
-    for a in range(1, limit):
-        for b in range(1, limit):
-            for c in range(1, limit):
-                if per_RightAngleTriangle(a, b, c) == 120:
-                    print(a, b, c)
-                    counter += 1
-    print(counter)
+    maxCounter = 0
+    result = 0
+    for p in range(2, 1000, 2):
+        counter = 0
+        for a in range(2, p // 3):
+            #(p² - 2pa) / (2p - 2a) if replace / on % and recieve 0 it will mean that b is correct for rigth angle triangle
+            if p * (p - 2 * a) % (2 * (p - a)) == 0: 
+                counter += 1
+            if counter > maxCounter:
+                maxCounter = counter
+                result = p
+    print(result)
 
 
 def main():
     start = t.time()
     program()
+    print(per_RightAngleTriangle(20,48,52))
     end = t.time()
     print("Total time taken:", end - start, "ms")
 
