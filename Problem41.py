77de83d053978daac1f61fbd0e140539457c3320
Egor Shatrov <xutpuu@gmail.com>
2019-05-01 22:57:08 +0300

    Problem 41 solved

--- Problem41.py
+++ Problem41.py
@@ -1,6 +1,8 @@
-# We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. 
+# We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once.
 # For example, 2143 is a 4-digit pandigital and is also prime.
 # What is the largest n-digit pandigital prime that exists?
+import math
+
 
 def benchmark(func):
     import time
@@ -15,9 +17,63 @@ def benchmark(func):
     return wrapper
 
 
+def primesList(limit):
+    crosslimit = math.sqrt(limit)
+    sieve = []
+    result = []
+
+    n = 1
+    for n in range(limit):
+        sieve.append(False)
+
+    n = 4
+    for n in range(n, limit, 2):
+        sieve[n] = True
+
+    n = 3
+    for n in range(n, int(crosslimit), 2):
+        if not sieve[n]:
+            m = n * n
+            for m in range(m, limit, 2 * n):
+                sieve[m] = True
+
+    n = 2
+    for n in range(n, limit, 1):
+        if not sieve[n]:
+            result.append(n)
+
+    return result
+
+
+def countDigits(n):
+    cnt = 0
+    while n > 0:
+        cnt += 1
+        n //= 10
+    return cnt
+
+
+def isUnusual(num):
+    sorted_str = "".join(sorted(num))
+    if sorted_str == "1234567":
+        return True
+    else:
+        return False
+
+
+def pandigital():
+    m = 0
+    primes = primesList(100000000)
+    for i in primes:
+        if isUnusual(str(i)) and i > m:
+            m = i
+    print(m)
+
+
 @benchmark
 def main():
     print("Begin")
+    pandigital()
 
 
 if __name__ == "__main__":
